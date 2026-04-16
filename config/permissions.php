5aaf6c38c5aa79ad0958737add8aa3d43aec8125
Darek Szulhaniuk <it@meblomaster.pl>
2026-04-16 11:56:11 +0200

    usuniecie permissions.php

--- config/permissions.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'dashboard' => ['dashboard.view'],
-    'users' => ['users.view', 'users.manage'],
-    'roles' => ['roles.view', 'roles.manage'],
-    'permissions' => ['permissions.view', 'permissions.manage'],
-];
